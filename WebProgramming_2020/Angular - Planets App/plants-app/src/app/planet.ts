00ce2fcd1951178eab59e237036e86aa39eb9ea8
102932079 <61527439+102932079@users.noreply.github.com>
2020-09-03 10:06:41 +1000

    Create a angular application called 'planets-app'.Create a planet class with the following properties:

---
+++ WebProgramming_2020/Angular - Planets App/plants-app/src/app/planet.ts
@@ -0,0 +1,13 @@
+export class Planet
+{
+    name: string
+    moons: number
+    distancefromSun: number
+
+    constructor(name:string, moons: number, distancefromSun: number)
+    {
+        this.name = name;
+        this.moons = moons;
+        this.distancefromSun = distancefromSun;
+    }
+}

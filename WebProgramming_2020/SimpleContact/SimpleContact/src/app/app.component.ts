ef2ba1483da2d7926a2001391581793dc05109a4
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-18 19:49:14 +1100

    finalise

--- WebProgramming_2020/SimpleContact/SimpleContact/src/app/app.component.ts
+++ WebProgramming_2020/SimpleContact/SimpleContact/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+//import class
+import { Contact } from './contact-component/contact';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'SimpleContact';
+  //title = 'SimpleContact';
+  //ngIf
+  showContact : boolean = true;
+
+  contact: string[] = ["Mr.", "Name5", "Address5"]
+
+  //  onToggle() : void{
+  //    this.showContacts = !this.showContacts;
+  //  }
 }

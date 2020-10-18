650963fdfb0fbaaa37f9160806a976420d666865
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-18 19:42:36 +1100

    define contact list

--- WebProgramming_2020/SimpleContact/SimpleContact/src/app/contact-list/contact-list.component.ts
+++ WebProgramming_2020/SimpleContact/SimpleContact/src/app/contact-list/contact-list.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+//import service
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
+
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ContactsService: ContactsService) { }
 
   ngOnInit(): void {
   }

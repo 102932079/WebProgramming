1eda8dc25d980d9bb599a0db962ffab2b2dd3ee2
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-18 19:30:28 +1100

    define contact component

--- WebProgramming_2020/SimpleContact/SimpleContact/src/app/contact-component/contact-component.component.ts
+++ WebProgramming_2020/SimpleContact/SimpleContact/src/app/contact-component/contact-component.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+//import class
+import { Contact } from './contact';
 
 @Component({
   selector: 'app-contact-component',
   templateUrl: './contact-component.component.html',
   styleUrls: ['./contact-component.component.css']
 })
-export class ContactComponentComponent implements OnInit {
+export class ContactComponentComponent {
+  //concept of inputs outputs properties
+  //Use the @Input() decorator in a child component or directive to let Angular know that a property in that component can receive its value from its parent component. It helps to remember that the data flow is from the perspective of the child component. So an @Input() allows data to be input into the child component from the parent component.
+  @Input()
+  contact : Contact;
 
   constructor() { }
 
-  ngOnInit(): void {
+  //ngOnInit(): void {  }
+
+  getUrl(){
+    return this.contact.ImgUrl;
   }
 
 }

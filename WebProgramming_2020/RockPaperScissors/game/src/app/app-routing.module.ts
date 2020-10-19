7553f257af7c91d7df980f718e3b319389c4e778
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-19 18:32:49 +1100

    test

--- WebProgramming_2020/RockPaperScissors/game/src/app/app-routing.module.ts
+++ WebProgramming_2020/RockPaperScissors/game/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import component
+import { PlayComponent } from './routes/play/play.component';
+import { ResultsComponent } from './routes/results/results.component';
 
-const routes: Routes = [];
+//define path for routes
+// this is where you app will be defined
+// deploy routes and define how we can access the routes in app-routing.module.ts using url as array
+
+const routes: Routes = [
+  // dynamic object
+  {path: "play", component: PlayComponent},
+  {path: "results", component: ResultsComponent},
+  // define another route here ** patrol routes
+  {path: "**", redirectTo: "play" }
+   // if we going to the home page of patrol it will automatically redirect to play compo
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebProgramming_2020/RockPaperScissors/game/src/app/app.module.ts
+++ WebProgramming_2020/RockPaperScissors/game/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlayComponent } from './play/play.component';
+import { PlayComponent } from './routes/play/play.component';
 import { ResultsComponent } from './routes/results/results.component';
 
 @NgModule({

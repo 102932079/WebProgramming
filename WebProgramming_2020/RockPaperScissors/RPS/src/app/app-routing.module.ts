896082f50799abff1ea593a65a78bc5d75d7eced
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-19 20:57:00 +1100

    define root component

--- WebProgramming_2020/RockPaperScissors/RPS/src/app/app-routing.module.ts
+++ WebProgramming_2020/RockPaperScissors/RPS/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import all component
+import { PlayComponent } from './routes/play/play.component';
+import { ResultComponent } from './routes/result/result.component';
 
-const routes: Routes = [];
+//define path for routes
+// this is where you app will be defined
+// deploy routes and define how we can access the routes in app-routing.module.ts using url as array
 
+const routes: Routes = [
+  // dynamic object
+  {path: "play", component: PlayComponent},
+  {path: "result", component: ResultComponent},
+  // define another route here ** patrol routes
+  {path: "**", redirectTo: "play" }
+   // if we going to the home page of patrol it will automatically redirect to play compo
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]

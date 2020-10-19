ea62a4c83fd4ef5a3600917362ed28bd0f661c0d
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-19 11:45:39 +1100

    init and picture

---
+++ WebProgramming_2020/RockPaperScissors/game/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PlayComponent } from './play/play.component';
+import { ResultsComponent } from './routes/results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlayComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

49df9ce911a8756ce55adb547532f98684aca969
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-20 19:33:46 +1100

    finalise

--- WebProgramming_2020/AngularConduit/conduit/src/app/article-list/article-list.component.ts
+++ WebProgramming_2020/AngularConduit/conduit/src/app/article-list/article-list.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+//import articleservice
+import { ArticlesService } from '../articles.service';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css']
 })
+
 export class ArticleListComponent implements OnInit {
 
-  constructor() { }
+  
+  arrtt: Article;
+
+  constructor(public articlesService: ArticlesService) { }
 
   ngOnInit(): void {
   }

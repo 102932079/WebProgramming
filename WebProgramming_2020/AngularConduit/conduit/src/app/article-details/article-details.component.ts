3ad4b92750d6cab72ab2fe19e3a912b97ca57d4a
102932079 <61527439+102932079@users.noreply.github.com>
2020-10-20 19:22:19 +1100

    define article detail comp

--- WebProgramming_2020/AngularConduit/conduit/src/app/article-details/article-details.component.ts
+++ WebProgramming_2020/AngularConduit/conduit/src/app/article-details/article-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { fromEventPattern } from 'rxjs';
+//import interface
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article-details',
@@ -6,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-details.component.css']
 })
 export class ArticleDetailsComponent implements OnInit {
+  //@Input() and @Output() allow Angular to share data between the parent context and child directives or components. An @Input() property is writable while an @Output() property is observable.
+  //Use the @Input() decorator in a child component or directive to let Angular know that a property in that component can receive its value from its parent component. It helps to remember that the data flow is from the perspective of the child component. So an @Input() allows data to be input into the child component from the parent component.
+  //import input and recall article interface
+  @Input()
+  arrtt: Article;
 
   constructor() { }
 

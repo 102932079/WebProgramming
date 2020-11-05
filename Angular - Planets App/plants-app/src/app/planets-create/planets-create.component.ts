8e1b9677aeb6a192538be4571fb1b49b7bcb6a81
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-05 21:07:55 +1100

    create comp ts

--- Angular - Planets App/plants-app/src/app/planets-create/planets-create.component.ts
+++ Angular - Planets App/plants-app/src/app/planets-create/planets-create.component.ts
@@ -11,18 +11,35 @@ import { Planet } from '../models/planet'
 })
 export class PlanetsCreateComponent implements OnInit {
 
-  constructor(public planetService: PlanetService) 
-  { 
+  createdplanet: Planet = new Planet("New Planet", 0, 200, "../../assets/Venus.png");
+  creating: boolean = false;
+  submitted: boolean = false;
 
-  }
+  constructor(public planetService: PlanetService) { }
 
-  ngOnInit(): void
+  ngOnInit(): void  { }
+
+  // createPlanet(name: string, moons: number, distToSun: number){
+  // this.planetService.creatPlanet(new Planet(name, moons, distToSun))
+  // }
+  public createPlanet()
   {
+    this.creating = true;
+    this.submitted = false;
   }
 
-  createPlanet(name: string, moons: number, distToSun: number)
-{
-  this.planetService.creatPlanet(new Planet(name, moons, distToSun))
-}
+  showFormControls(form: any)
+  {
+    return form && form.controls['name'] && form.controls['name'].value;
+  }
+
+  onSubmit()
+  {
+    this.submitted = true;
+    this.creating = false;
+    console.log(this.createdplanet);
+    this.planetService.planets.push(this.createdplanet);
+    this.createdplanet = new Planet("New Planet", 0, 200, "../../assets/Venus.png");
+  }
 
 }

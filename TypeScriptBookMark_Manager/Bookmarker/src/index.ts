15150ab9402ea3ece8f001571ec8ff7c46ad899f
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-05 12:24:58 +1100

    starting project

--- TypeScriptBookMark_Manager/Bookmarker/src/index.ts
+++ TypeScriptBookMark_Manager/Bookmarker/src/index.ts
@@ -1,32 +1,35 @@
-import { Todo, TodoElement } from "./todo";
-//import class from src
+//===========================DEMO==================================
+// import { Todo, TodoElement } from "./todo";
+// //import class from src
 
-let todoContainer = document.getElementById("todoContainer") as HTMLDivElement | null;//check null for every element variable
+// let todoContainer = document.getElementById("todoContainer") as HTMLDivElement | null;//check null for every element variable
 
-let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
+// let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
 
-if (myButton == null) {
-    alert("Button not found.");
-} else {
+// if (myButton == null) {
+//     alert("Button not found.");
+// } else {
     
-    //group method together as a class(component)
-    myButton.onclick = function () {
-        if(myButton ==  null || todoContainer == null) return; //undefined
-        //alert("Hello World!");
+//     //group method together as a class(component)
+//     myButton.onclick = function () {
+//         if(myButton ==  null || todoContainer == null) return; //undefined
+//         //alert("Hello World!");
 
-        let todo = new Todo("Example Title");
+//         let todo = new Todo("Example Title");
 
-        let todoElement = new TodoElement(todo);
-        todoElement.spawn(todoContainer); // "todoContainer"
+//         let todoElement = new TodoElement(todo);
+//         todoElement.spawn(todoContainer); // "todoContainer"
 
-        todo.push(todo);//todoElement
+//         todo.push(todo);//todoElement
 
-        //manipulate the element
-        todo.completed = true;
+//         //manipulate the element
+//         todo.completed = true;
 
-        //call the delete after alert
-        // alert();
-        // todoElement.delete(todoContainer); // "todoContainer"
-        console.log(todo);
-    }
-}
+//         //call the delete after alert
+//         // alert();
+//         // todoElement.delete(todoContainer); // "todoContainer"
+//         console.log(todo);
+//     }
+// }
+
+//================================================================================================

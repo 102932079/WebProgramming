41e53eeb6647d2db823dbe2de40b1a25af242e7b
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-06 14:16:34 +1100

    move repo

--- RockPaperScissors/RPS/src/app/models/serverResonse.ts
+++
@@ -1,6 +0,0 @@
-//json property name attribute
-export interface serverResponse {
-    playerChoice: string,
-    cpuChoice: string,
-    result: string;
-}
--- RockPaperScissors/RPS/src/app/models/userSelection.ts
+++
@@ -1,3 +0,0 @@
-export interface userSelection {
-    userChoice:string;
-}

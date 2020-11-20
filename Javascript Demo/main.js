763aaa51da63ecfbcf47396cd94a4b570410a2e3
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-20 19:34:18 +1100

    JS intro

--- Javascript Demo/main.js
+++ Javascript Demo/main.js
@@ -1,13 +1,4 @@
-//`Definition
 
-//JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles. Read more about JavaScript.
 
-//`non-browser environments also use it, such as Node.js
-//`compared with C# JS is very different. C# is a complied based language mean executing command and processing to do sth the program has to complied to machine code
-//`But JS can execute straightaway
+    
 
-//Java and Javascript are also different. Java and c# are C based
-
-//empty tag then use Dev tools(F12) We can execute the JS command in browser console
-
-//console.log() lower case and instead of writeline, clear() method

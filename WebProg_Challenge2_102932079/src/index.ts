c9613b895ebf5521bde9e82873465177f0071712
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-25 18:47:26 +1100

    import class in index and create variable for display

--- WebProg_Challenge2_102932079/src/index.ts
+++ WebProg_Challenge2_102932079/src/index.ts
@@ -1,1 +1,8 @@
-// `the collection of js (import export) need go create class first
+//the collection of js  need go create class first with export
+//remember to import my class
+import { Quiz, Package } from "./model";
+
+//`create a variable for toggle the target div element (display)
+let display = document.getElementsByClassName("display"); //may need to change the display from class to id
+
+//`start to insert questions text

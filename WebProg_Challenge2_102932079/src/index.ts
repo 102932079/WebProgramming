68a9146127f05aa83242d63a3262c2989b8e289a
102932079 <61527439+102932079@users.noreply.github.com>
2020-11-26 13:28:08 +1100

    forgot number for questions

--- WebProg_Challenge2_102932079/src/index.ts
+++ WebProg_Challenge2_102932079/src/index.ts
@@ -8,26 +8,26 @@ let display = document.getElementById("display") as HTMLDivElement; //may need t
 //!  as HTMLElement
 
 //`start to insert questions text /question option1 option2 option3 option4 answerNO
-let Quiz1 = new Quiz("What is the rarest blood type?", "a)O", "b)A", "c)B", "d)AB",4);
+let Quiz1 = new Quiz("1. What is the rarest blood type?", "a)O", "b)A", "c)B", "d)AB",4);
 let Package1 = new Package(Quiz1);
 //`make the package1 spawn in display div zone
 Package1.spawn(display);
 //!some issue with the display variable try id
 //Argument of type 'HTMLCollectionOf<Element>' is not assignable to parameter of type 'HTMLElement'.Type 'HTMLCollectionOf<Element>' is missing the following properties from type 'HTMLElement': accessKey, accessKeyLabel, autocapitalize, dir, and 234 more.ts(2345)
 
-let Quiz2 = new Quiz("How many bones are there in the human body?", "a)206", "b)205", "c)201", "d)209",1);
+let Quiz2 = new Quiz("2. How many bones are there in the human body?", "a)206", "b)205", "c)201", "d)209",1);
 let Package2 = new Package(Quiz2);
 Package2.spawn(display);
 
-let Quiz3 = new Quiz("Fe is the chemical symbol for…", "a)Zinc", "b)Hydrogen", "c)Fluorine", "d)Iron",4);
+let Quiz3 = new Quiz("3. Fe is the chemical symbol for…", "a)Zinc", "b)Hydrogen", "c)Fluorine", "d)Iron",4);
 let Package3 = new Package(Quiz3);
 Package3.spawn(display);
 
-let Quiz4 = new Quiz("Which planet in our solar system is the largest?", "a)Jupiter", "b)Saturn", "c)Neptune", "d)Earth",4);
+let Quiz4 = new Quiz("4. Which planet in our solar system is the largest?", "a)Jupiter", "b)Saturn", "c)Neptune", "d)Earth",4);
 let Package4 = new Package(Quiz4);
 Package4.spawn(display);
 
-let Quiz5 = new Quiz("Which ocean is the largest?", "a)Indian", "b)Pacific", "c)Atlantic", "d)Arctic",2);
+let Quiz5 = new Quiz("5. Which ocean is the largest?", "a)Indian", "b)Pacific", "c)Atlantic", "d)Arctic",2);
 let Package5 = new Package(Quiz5);
 Package5.spawn(display);
 
